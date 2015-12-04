a9e628869688e3f2dbfc52730a05f69b782bee99
Farshid Tavakolizadeh <tavakoli@fit.fraunhofer.de>
2015-12-04 23:53:47 +0100

    formatting

--- motor.c
+++ motor.c
@@ -1,18 +1,18 @@
 /*
-	Movement functions
+Movement functions
 */
 
 // Move per gear tooth
-void driveDistance(int noOfNipples , int power, short motorB)
+void driveDistance(int noOfNipples , int power, short motorX)
 {
 
 	int tickGoal= noOfNipples * 15*2.5 ; // gear has 24 head = 24mm which is equal to 360*; each nipple is equal to 2,5mm
 
-	nMotorEncoder[motorB] = 0;  //clear the LEGO motor encoders
-  nMotorEncoderTarget[motorB] = tickGoal; //set the target stoping position
-  motor[motorB] = power;
+	nMotorEncoder[motorX] = 0;  //clear the LEGO motor encoders
+	nMotorEncoderTarget[motorX] = tickGoal; //set the target stoping position
+	motor[motorX] = power;
 
-  while (nMotorRunState[motorB] != runStateIdle){}
-  motor[motorB] = 0; //turn both motors off
+	while (nMotorRunState[motorX] != runStateIdle){}
+	motor[motorX] = 0; //turn motor off
 
 }
--- printer.c
+++ printer.c
@@ -4,25 +4,25 @@ void setBrick(int i, int j)
 {
 
 	driveDistance(7,-30,motorB);
-  wait1Msec(500);              //Stop in between each command to prevent momentum causing wheel skid.
-  driveDistance(7,30,motorB);
-  wait1Msec(500);
+	wait1Msec(500);							 //Stop in between each command to prevent momentum causing wheel skid.
+	driveDistance(7,30,motorB);
+	wait1Msec(500);
 
 	driveDistance(i+5 ,-30,motorC);
-  wait1Msec(500);
+	wait1Msec(500);
 
-  //driveDistance(j ,-30,motorC); // for the 2D movement
-  //wait1Msec(500);
+	//driveDistance(j ,-30,motorC); // for the 2D movement
+	//wait1Msec(500);
 
-  driveDistance(8,-90,motorB);
-  wait1Msec(500);              //Stop in between each command to prevent momentum causing wheel skid.
-  driveDistance(8,30,motorB);
-  wait1Msec(500);
+	driveDistance(8,-90,motorB);
+	wait1Msec(500);							 //Stop in between each command to prevent momentum causing wheel skid.
+	driveDistance(8,30,motorB);
+	wait1Msec(500);
 
-  driveDistance(i+5 , 30,motorC);
-  wait1Msec(500);
-  //driveDistance(j ,30,motorC);
-  //wait1Msec(500);
+	driveDistance(i+5 , 30,motorC);
+	wait1Msec(500);
+	//driveDistance(j ,30,motorC);
+	//wait1Msec(500);
 
 }
 
@@ -31,31 +31,31 @@ void setBrick(int i, int j)
 //	int s= letter.length;
 
 // for(int i = 0; i< s; i++)
-//   {
-//     if (IntArray[i]==1)
-//     {
-//       setBrick(i,0);
-//       }
-// 	}
+//	 {
+//		 if (IntArray[i]==1)
+//		 {
+//			 setBrick(i,0);
+//			 }
+//	}
 
 //}
 
 task main()
 {
 
-int IntArray[4] =
-{
-    1,
-    0,
-    1,
-    1,
-};
+	int IntArray[4] =
+	{
+		1,
+		0,
+		1,
+		1,
+	};
 
- for(int i = 0; i< 4; i++)
-   {
-     if (IntArray[i]==1)
-     {
-       setBrick(i,0);
-       }
- 	}
+	for(int i = 0; i< 4; i++)
+	{
+		if (IntArray[i]==1)
+		{
+			setBrick(i,0);
+		}
+	}
 }
