779c946310764f36cee5f7f45d98512c2411637a
Farshid Tavakolizadeh <tavakoli@fit.fraunhofer.de>
2016-01-02 18:21:52 +0100

    refactoring

--- motor.c
+++ motor.c
@@ -2,44 +2,30 @@
 Movement functions
 */
 
+// Move per gear degree
+void driveDegree(int degree , int power, short motorX)
+{
+	nMotorEncoder[motorX] = 0;  //clear the motor encoder
+	nMotorEncoderTarget[motorX] = degree; //set the target stoping position
+	motor[motorX] = power;
+
+	while (nMotorRunState[motorX] != runStateIdle){} // wait till it reaches the target
+	motor[motorX] = 0; //turn motor off
+}
+
 // Move per nipple
 void driveNipple(float noOfNipples , int power, short motorX)
 {
 	float gearToNipple = 12/2.5; // each nipple is 2.5 teeth
-	int tickGoal= (360*noOfNipples)/gearToNipple;
+	int target = (360*noOfNipples)/gearToNipple;
 
-	nMotorEncoder[motorX] = 0;  //clear the LEGO motor encoders
-	nMotorEncoderTarget[motorX] = tickGoal; //set the target stoping position
-	motor[motorX] = power;
-
-	while (nMotorRunState[motorX] != runStateIdle){}
-	motor[motorX] = 0; //turn motor off
-	PlaySound(soundBlip);
+	driveDegree(target, power, motorX);
 }
 
 // Move per gear tooth
-void driveGear(float teeth , int power, short motorX, float gearBox)
+void driveGear(float teeth , int power, short motorX, float gearRatio)
 {
+	int target = (360*teeth)/gearRatio;
 
-	int tickGoal= (360*teeth)/gearBox;
-
-	nMotorEncoder[motorX] = 0;  //clear the LEGO motor encoders
-	nMotorEncoderTarget[motorX] = tickGoal; //set the target stoping position
-	motor[motorX] = power;
-
-	while (nMotorRunState[motorX] != runStateIdle){}
-	motor[motorX] = 0; //turn motor off
-  PlaySound(soundBlip);
-}
-
-// Move per gear degree
-void driveDegree(int degree , int power, short motorX)
-{
-	nMotorEncoder[motorX] = 0;  //clear the LEGO motor encoders
-	nMotorEncoderTarget[motorX] = degree; //set the target stoping position
-	motor[motorX] = power;
-
-	while (nMotorRunState[motorX] != runStateIdle){}
-	motor[motorX] = 0; //turn motor off
-  PlaySound(soundBlip);
+	driveDegree(target, power, motorX);
 }
